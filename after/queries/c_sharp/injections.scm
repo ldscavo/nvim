e1c8d58ece9faaf46bdd9099b48c42ea1519337f
Logan Scavo <logan@scavo.dev>
2025-04-03 13:24:05 -0400

    another sql into csharp injection case

--- after/queries/c_sharp/injections.scm
+++ after/queries/c_sharp/injections.scm
@@ -7,3 +7,11 @@
    (#contains? @_name "query")
    (#offset! @injection.content 0 2 0 -1)
    (#set! injection.language "sql"))
+
+((variable_declarator
+   name: (identifier) @_name 
+   (verbatim_string_literal) @injection.content)
+   .
+   (#match? @_name "[sS][qQ][lL]")
+   (#offset! @injection.content 0 2 0 -1)
+   (#set! injection.language "sql"))
