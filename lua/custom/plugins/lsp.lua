375a2db8b0db1f94d7b77ed5c23798045941f674
Logan Scavo <logan@scavo.dev>
2025-04-04 08:52:03 -0400

    reinstate disappearing work js formatting rules

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -180,19 +180,6 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
-
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
@@ -207,13 +194,28 @@ return {
           },
         },
       },
-      eslint = {},
       fsautocomplete = {},
       pyright = {},
       csharp_ls = {},
       cssls = {},
       powershell_es = {},
       terraformls = {},
+      ts_ls = {
+        settings = {
+          javascript = {
+            format = {
+              insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
+              insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = false,
+            },
+          },
+          typescript = {
+            format = {
+              insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
+              insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = false,
+            },
+          },
+        },
+      },
     }
 
     -- Ensure the servers and tools above are installed
