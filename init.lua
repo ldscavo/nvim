7f2b3538a30dcd86a0e4a39b368fbd12d3680a86
Logan Scavo <logan@scavo.dev>
2025-10-14 15:04:00 -0400

    sessions

--- init.lua
+++ init.lua
@@ -349,6 +349,10 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.starter').setup()
+      -- Better sessions
+      require('mini.sessions').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
