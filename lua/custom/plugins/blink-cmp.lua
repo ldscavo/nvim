e48a53be60d54024477ce9e42ff62e98b52c592e
Logan Scavo <logan@scavo.dev>
2025-10-14 15:02:35 -0400

    migrate to blink-cmp

---
+++ lua/custom/plugins/blink-cmp.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+
+      completion = {
+        menu = { border = 'single' },
+        documentation = {
+          auto_show = false,
+          auto_show_delay_ms = 500,
+        },
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        },
+      },
+
+      snippets = { preset = 'luasnip' },
+
+      fuzzy = { implementation = 'lua' },
+
+      signature = {
+        enabled = true,
+        trigger = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
