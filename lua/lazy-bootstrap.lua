cb34ae72cfaa728e8141b8da4c90374671da1c79
Logan Scavo <lscavo@connorsllc.com>
2025-12-12 16:36:02 -0500

    remove unneeded comment blocks

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -8,17 +8,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   { import = 'plugins' },
 }, {
